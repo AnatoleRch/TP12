a07ed3dfe09d7b727f1558ffdbd28f5cabe378fe
Remi Bastide <remi.bastide@univ-jfc.fr>
2023-11-22 16:14:49 +0100

    aa

--- src/test/java/bowling/MultiPlayerTest.java
+++ src/test/java/bowling/MultiPlayerTest.java
@@ -40,12 +40,31 @@ class MultiPlayerTest {
         PartieMultiJoueurs partie = new PartieMultiJoueurs();
         String [] monTableau = {"Pierre", "Paul"};
         assertEquals(partie.demarreNouvellePartie(monTableau),"Prochain tir : joueur Pierre, tour n° 1, boule n° 1");
-        assertEquals(partie.enregistreLancer(5),"Prochain tir : joueur Pierre, tour n° 1, boule n° 2");
-        assertEquals(partie.enregistreLancer(3),"Prochain tir : joueur Paul, tour n° 1, boule n° 1");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Paul, tour n° 1, boule n° 1");
         assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Pierre, tour n° 2, boule n° 1");
-        assertEquals(partie.enregistreLancer(7),"Prochain tir : joueur Pierre, tour n° 2, boule n° 2");
-        assertEquals(partie.enregistreLancer(3),"Prochain tir : joueur Paul, tour n° 2, boule n° 1");
-        assertEquals(partie.scorePour("Pierre"),18);
-        assertEquals(partie.scorePour("Paul"),10);
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Paul, tour n° 2, boule n° 1");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Pierre, tour n° 3, boule n° 1");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Paul, tour n° 3, boule n° 1");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Pierre, tour n° 4, boule n° 1");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Paul, tour n° 4, boule n° 1");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Pierre, tour n° 5, boule n° 1");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Paul, tour n° 5, boule n° 1");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Pierre, tour n° 6, boule n° 1");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Paul, tour n° 6, boule n° 1");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Pierre, tour n° 7, boule n° 1");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Paul, tour n° 7, boule n° 1");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Pierre, tour n° 8, boule n° 1");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Paul, tour n° 8, boule n° 1");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Pierre, tour n° 9, boule n° 1");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Paul, tour n° 9, boule n° 1");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Pierre, tour n° 10, boule n° 1");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Pierre, tour n° 10, boule n° 2");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Pierre, tour n° 10, boule n° 3");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Paul, tour n° 10, boule n° 1");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Paul, tour n° 10, boule n° 2");
+        assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Paul, tour n° 10, boule n° 3");
+        assertEquals(partie.enregistreLancer(10),"Partie terminée");
+        assertEquals(300,partie.scorePour("Pierre"));
+        assertEquals(300,partie.scorePour("Paul"));
     }
 }
